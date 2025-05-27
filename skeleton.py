760635cedd5431ca0194b8a016d61489ec40f455
angelkautsi <angelkautsi09@gmail.com>
2025-05-27 07:58:13 +0200

    added skeleton

---
+++ skeleton.py
@@ -0,0 +1,6 @@
+import wx
+app = wx.App() #starts the main loop
+frame = wx.Frame(parent=None, title='HELLO ANGEL') #creates interaction window
+frame.Show() #shows the window
+app.MainLoop() #executes the mainloop
+
